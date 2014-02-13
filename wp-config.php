3c10ea2e17b550d2f5bbc5f8912f23d995bbc1fd
Thomas Brodin <info@hotelexistence.com>
2014-02-13 09:37:52 -0500

    salt

--- wp-config.php
+++ wp-config.php
@@ -29,14 +29,14 @@ define( 'DB_COLLATE', '' );
 // Salts, for security
 // Grab these from: https://api.wordpress.org/secret-key/1.1/salt
 // ==============================================================
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         '-+ICwkH$8qQ+Lj C74)J+[{HlI-?*Az/._vA%ScJ|B]e-|7;Y52((qaUj=>M,za6');
+define('SECURE_AUTH_KEY',  'HY},&0Z92p:fQ7 ,!El-Zgod8+q7[uG;0h{4u~]ClY.tW>;4PG`oZ=%::/u6[ze&');
+define('LOGGED_IN_KEY',    '-cAx:zK^s:9KporWy5Jm<;<NT]IcF?i,k<=1+,A#+|E&5`*vUWl&vm8IR23`WsL~');
+define('NONCE_KEY',        'f[/>Axsk+d#xoAg1moX`;NQ#^Iz!+TIN-hWP^:ZAG,{e#x|VH+$mZ:- G~#@B&6<');
+define('AUTH_SALT',        'Vg}n:{o(yERdD5M9]a-f%X34{5t(>#Ly|P V]@q($LAhA) , !p[y|&g3e,#JS+@');
+define('SECURE_AUTH_SALT', 'zPGR.I#!ApcB}=>MSP~ETH]!oj92<n6 (ML!P3w7~(,Fff_R@>A|MSB0%<WEt=>4');
+define('LOGGED_IN_SALT',   'Ib+3ciq+z$.XgZ4%%ht*2R--wucz`srX8)(%FScG)q6;Y`7?%@iei[$09OE}a *]');
+define('NONCE_SALT',       ']G$7@wzl$qgvpT*.qiO+9i&#OG5zS[PdF,j{9WI2+9[Y6r<E1D+OZ5e+9e5^0Bx_');
 
 // ==============================================================
 // Table prefix
