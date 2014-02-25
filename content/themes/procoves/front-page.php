62b1b0f9ce6185ef9098035b8ecaa91bf83c7364
Thomas Brodin <info@hotelexistence.com>
2014-02-25 16:18:05 -0500

    Home page Template

--- content/themes/procoves/front-page.php
+++ content/themes/procoves/front-page.php
@@ -1,10 +1,6 @@
 <?php
 /**
- * The main template file
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file
+ * The template for Front Page
  *
  * Methods for TimberHelper can be found in the /functions sub-directory
  *
@@ -17,7 +13,16 @@
 		echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
 	}
 	$context = Timber::get_context();
-	$context['slogan'] = 'Procoves releve le gant de la performance';
+	
+	$frontID = get_option('page_on_front');
+	$childs = get_pages( array( 'child_of' => $frontID,'sort_column' => 'menu_order') );
+	$context['concep'] = Timber::get_post($childs[0]->ID);
+	$context['fab'] = Timber::get_post($childs[1]->ID);
+	$context['preco'] = Timber::get_post($childs[2]->ID);
+	
+	$context['images'] = get_field('home_slide');
+	$context['slogan'] = get_field('accroche');
+	
 	Timber::render('front-page.twig', $context);
 
 
