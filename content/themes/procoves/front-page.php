e608672f3ace7f4de8e8090c392ef3c745c88cb4
Thomas Brodin <info@hotelexistence.com>
2014-02-26 22:50:24 -0500

    Actu post & Vedette tax query

--- content/themes/procoves/front-page.php
+++ content/themes/procoves/front-page.php
@@ -12,10 +12,27 @@
 	if (!class_exists('Timber')){
 		echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
 	}
+
 	$context = Timber::get_context();
 	
+	$args = array(
+		'post_type' => 'produits', 
+		'numberposts' => 12,
+		'tax_query' => array(
+            array(
+                'taxonomy' => 'mots_cles',
+                'field' => 'slug',
+                'terms' => array( 'vedette' )
+            ),
+        )
+	);
+	$context['produits'] = Timber::get_posts($args);
+	
+	$context['actu'] = Timber::get_post('category_name=a-la-une');
+
 	$frontID = get_option('page_on_front');
 	$childs = get_pages( array( 'child_of' => $frontID,'sort_column' => 'menu_order') );
+
 	$context['concep'] = Timber::get_post($childs[0]->ID);
 	$context['fab'] = Timber::get_post($childs[1]->ID);
 	$context['preco'] = Timber::get_post($childs[2]->ID);
